7d4103b335488bbde08dfa66a7ba752c225ff7e8
Abraão Silva <abraaojr1996@gmail.com>
2022-03-25 17:03:38 -0300

    added instructions.md to .gitignore

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+
+const initialTimeInSeconds = 25 * 60;	// equals 1500 seconds or 25 minutes
+// const initialTimeInSeconds = 25 * 60 - 4;	// test: 4 seconds passed
+// const initialTimeInSeconds = 5;	// test: 5 seconds only
 
 function App() {
+  const [totalSeconds, setTotalSeconds] = useState(initialTimeInSeconds);
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+
+  // why do you never attribute useEffect() to a variable?
+  useEffect(() => {
+    if (totalSeconds > 0) {
+      setTimeout(() => {
+        setTotalSeconds(state => state - 1);
+      }, 1000)
+    } else {
+      alert("TIME'S UP");
+    }
+  }, [totalSeconds]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span>{String(minutes).padStart(2, '0')}</span>
+      <span>:</span>
+      <span>{String(seconds).padStart(2, '0')}</span>
+      <button>Begin</button>
+      <button>Pause</button>
     </div>
-  );
+  )
 }
 
 export default App;
